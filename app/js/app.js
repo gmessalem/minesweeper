cb3ec55ac77e46cb4793fb0a9ed20698dcc04328
Guy Messalem <gmessalem@gmail.com>
2015-01-23 13:55:29 +0200

    working baseline

---
+++ app/js/app.js
@@ -0,0 +1,53 @@
+(function(angular) {
+    'use strict';
+
+    var app = angular.module('puzzleApp', ['slidingPuzzle']);
+
+    // puzzle types
+    var types = [
+        { id: 'sliding-puzzle', title: 'Sliding puzzle' },
+    ];
+
+    /**
+     * Config
+     */
+    app.config(function($routeProvider) {
+        $routeProvider.when('/:type');
+    });
+
+    /**
+     * Startup
+     */
+    app.run(function($rootScope, $route, $filter) {
+        $rootScope.types = types;
+        $rootScope.type = types[0].id;
+
+        // set type on route change
+        $rootScope.$on('$routeChangeSuccess', function(event, route) {
+            $rootScope.type = ($filter('filter')(types, { id: route.params.type }).length ? route.params.type : types[0].id);
+        });
+    });
+
+    /**
+     * Advanced sliding puzzle controller
+     */
+    app.controller('slidingAdvancedCtrl', function($scope) {
+        $scope.puzzles = [
+            { src: './img/misko.jpg', rows: 10, cols: 10 }
+        ];
+    });
+
+    app.directive('ngRightClick', function($parse) {
+        return function(scope, element, attrs) {
+            var fn = $parse(attrs.ngRightClick);
+            element.bind('contextmenu', function(event) {
+                scope.$apply(function() {
+                    event.preventDefault();
+                    fn(scope, {$event:event});
+                });
+            });
+        };
+    });
+
+
+})(window.angular);
